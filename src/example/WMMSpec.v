8de0f70e9fc7f4fdbf4ab732a4dc78a72b99900e
SB1491 <etaeyoung2@snu.ac.kr>
2024-06-04 20:53:23 +0900

    2406042053

--- src/example/WMMSpec.v
+++ src/example/WMMSpec.v
@@ -55,6 +55,32 @@ Context `{AUTHVWRA: @GRA.inG (Auth.t (Excl.t View.t)) Σ}.
 Context `{AUTHVWRA2: @GRA.inG (Auth.t (Excl.t (View.t * unit))) Σ}.
 Context `{AUTHNMNRA: @GRA.inG (Auth.t (NatMapRALarge.t nat)) Σ}.
 
+
+Lemma wload_fun_spec'
+l v vw vw'
+tid E R_src R_tgt (Q : R_src -> R_tgt -> iProp)
+r g ps pt
+itr_src ktr_tgt
+st0 mem 
+:
+(St_tgt (st0, mem))
+-∗
+(wpoints_to l v vw)
+-∗
+(wmemory_black mem)
+-*
+(⌜View.le vw vw'⌝)
+-∗
+(∀vw'', St_tgt (st0, mem) -∗ (wpoints_to l v vw'') -∗ (⌜View.le vw' vw''⌝)
+-∗
+(wmemory_black mem)-∗ stsim tid E r g Q ps true itr_src (ktr_tgt (vw'', v)))
+-∗
+(stsim tid E r g Q ps pt itr_src
+(map_event (OMod.emb_callee tgt_mod (WMem.mod)) (WMem.load_fun (vw', l, Ordering.plain)) >>= ktr_tgt))
+.
+
+Abort.
+
 Lemma wload_fun_spec
 l v vw vw'
 tid E R_src R_tgt (Q : R_src -> R_tgt -> iProp)
@@ -70,7 +96,8 @@ st0 mem
 -*
 (⌜View.le vw vw'⌝)
 -∗
-(∀vw'', St_tgt (st0, mem) -∗ (wpoints_to l v vw'') -∗
+(∀vw'', St_tgt (st0, mem) -∗ (wpoints_to l v vw'') -∗ (⌜View.le vw' vw''⌝)
+-∗
 (wmemory_black mem)-∗ stsim tid E r g Q ps true itr_src (ktr_tgt (vw'', v)))
 -∗
 (stsim tid E r g Q ps pt itr_src
@@ -88,7 +115,7 @@ iPoseProof (wpoints_to_view_mon with "WPOINTS_TO") as "WPOINTS_TO". eapply VW.
 iPoseProof (wmemory_ra_load with "MEM_BLACK WPOINTS_TO") as "[i1MEM [%VW2 >i0PTR]]".
 eapply READ. eauto. eauto. des. subst val.
 iApply stsim_fairR.
-{ i. instantiate (1:= []). ss. clear - IN.
+{  i. instantiate (1:= []). ss. clear - IN.
 unfold prism_fmap, WMem.missed in IN. des_ifs.
 }
 { i. instantiate (1:=[]) in IN. inv IN. }
@@ -103,6 +130,7 @@ iFrame.
 iPoseProof ("Q" with "[i0PTR]") as "Q".
 iFrame.
 iPoseProof ("Q" with "[i1MEM]") as "Q".
+iFrame.
 iFrame.
 iFrame.
 Qed.
