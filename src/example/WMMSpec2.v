e2b6177a8e78e6d263d57a7a2a6d4bba4c8fa6e9
SB1491 <etaeyoung2@snu.ac.kr>
2024-06-11 12:28:44 +0900

    2406111228

--- src/example/WMMSpec2.v
+++ src/example/WMMSpec2.v
@@ -3,7 +3,7 @@ From Paco Require Import paco.
 From stdpp Require namespaces.
 Require Import Coq.Classes.RelationClasses Lia Program.
 From Fairness Require Export ITreeLib
-     PCM IProp IPM WMM FairLock
+     PCM IProp IPM WMM FairLock TicketLockW
      Mod Linking SCM Red Weakest FancyUpdate.
      From PromisingLib Require Import Loc Event.
      From PromisingSEQ Require Import Time View TView Cell Memory Local.
@@ -11,25 +11,34 @@ From Fairness Require Export ITreeLib
 From Fairness Require Import NatStructs.
 Section WSPEC.
 
-
 Variable ident_src: ID.
 Variable state_src: Type.
 
 Variable gvars : list nat.
-Variable tgt_mod : Mod.t.
 
+Variable funs : fname -> option (ktree (threadE void ()) Any.t Any.t) .
+
+Definition tgt_mod funs : Mod.t :=
+     {|
+          Mod.state := ();
+          Mod.ident := void;
+          Mod.st_init := ();
+          Mod.funs := funs;
+     |}.
+
+Definition tgt_mod' := tgt_mod funs.
 Context `{Σ: GRA.t}.
 
 Context `{Invs : @InvSet Σ}.
 
 Context `{MONORA: @GRA.inG monoRA Σ}.
 Context `{THDRA: @GRA.inG ThreadRA Σ}.
-Context `{STATESRC: @GRA.inG (stateSrcRA (Mod.state tgt_mod)) Σ}.
-Context `{STATETGT: @GRA.inG (stateTgtRA (OMod.closed_state tgt_mod (WMem.mod))) Σ}.
-Context `{IDENTSRC: @GRA.inG (identSrcRA (Mod.ident tgt_mod)) Σ}.
-Context `{IDENTTGT: @GRA.inG (identTgtRA (OMod.closed_ident tgt_mod (WMem.mod))) Σ}.
+Context `{STATESRC: @GRA.inG (stateSrcRA (Mod.state (tgt_mod'))) Σ}.
+Context `{STATETGT: @GRA.inG (stateTgtRA (OMod.closed_state tgt_mod' (WMem.mod))) Σ}.
+Context `{IDENTSRC: @GRA.inG (identSrcRA (Mod.ident tgt_mod')) Σ}.
+Context `{IDENTTGT: @GRA.inG (identTgtRA (OMod.closed_ident tgt_mod' (WMem.mod))) Σ}.
 
-Context `{ARROWRA: @GRA.inG (ArrowRA (OMod.closed_ident tgt_mod (WMem.mod))) Σ}.
+Context `{ARROWRA: @GRA.inG (ArrowRA (OMod.closed_ident tgt_mod' (WMem.mod))) Σ}.
 (*
 Context `{STATETGT: @GRA.inG (stateTgtRA ((OMod.closed_state ClientImpl.omod (ModAdd (WMem.mod) AbsLockW.mod)))) Σ}.
 Context `{IDENTTGT: @GRA.inG (identTgtRA (OMod.closed_ident ClientImpl.omod (ModAdd (WMem.mod) AbsLockW.mod))%type) Σ}.
@@ -54,6 +63,90 @@ Context `{AUTHVWRA: @GRA.inG (Auth.t (Excl.t View.t)) Σ}.
 Context `{AUTHVWRA2: @GRA.inG (Auth.t (Excl.t (View.t * unit))) Σ}.
 Context `{AUTHNMNRA: @GRA.inG (Auth.t (NatMapRALarge.t nat)) Σ}.
 
+
+
+Lemma wload_fun_spec''
+l v vw vw' V k P Q'
+tid E R_src R_tgt (Q : R_src -> R_tgt -> iProp)
+r g ps pt
+itr_src ktr_tgt
+st0 (mem: WMem.t)
+:
+(St_tgt (st0, mem))
+-∗
+(wpoints_to_full l V k P Q')
+-∗
+(wpoints_to l v vw)
+-∗
+(wmemory_black_strong mem)
+-∗
+(⌜View.le vw vw'⌝)
+-∗
+(∀vw'', 
+St_tgt (st0, mem) -∗ 
+(wpoints_to_full l V k P Q')
+-∗
+(wpoints_to l v vw'')
+-∗
+∃ j , FairRA.white (j) 1
+-∗
+(⌜View.le vw' vw''⌝) -∗ (wmemory_black_strong mem)-∗ 
+stsim tid E r g Q ps true itr_src (ktr_tgt (vw'', v)))
+-∗
+
+(stsim tid E r g Q ps pt itr_src
+(map_event (OMod.emb_callee tgt_mod' (WMem.mod)) (WMem.load_fun (vw', l, Ordering.acqrel)) >>= ktr_tgt))
+.
+iIntros "ST_TGT WPOINTS_TO_FULL WPOINTS_TO WMEM %VW Q".
+rred. iApply stsim_getR.
+iSplit. iFrame.
+rred.
+iApply stsim_chooseR. iIntros "%".
+destruct x. destruct x as [[lc1 val] to]. des. rename y into READ. rred.
+iPoseProof (wpoints_to_view_mon with "WPOINTS_TO") as "WPOINTS_TO". eapply VW.
+
+
+iPoseProof (wmemory_ra_load_acq with "WMEM WPOINTS_TO_FULL") as "[%RVLE [MEM0 [MEM1 WCASES]]]".
+eapply READ. eauto. eauto. des.
+
+iDestruct "WCASES" as "[[%WP [% [#WCOR %MISSED]]] | [%WQ %VVLE]]".
+
+{ iApply stsim_fairR.
+      { i. instantiate (1:=[]). exfalso. clear - IN. unfold prism_fmap, WMem.missed in IN. des_ifs. }
+      { i. instantiate (1:=[inr (l, ts)]) in IN . inv IN. ss. inv H. }
+      { econs. ii. inv H. econs. }
+      { ss. }
+      iIntros "_ WHITES".
+      iDestruct "WHITES" as "[WHITE _]". clear MISSED.
+      rred. 
+      iPoseProof ("Q" with "[ST_TGT]") as "Q".
+iFrame.
+iPoseProof ("Q" with "[MEM1]") as "Q".
+iFrame.
+iPoseProof ("Q" with "[WPOINTS_TO]") as "Q".
+iFrame.
+i
+iPoseProof ("Q" with "[WHITE]") as "Q".
+iFrame.
+
+      }
+
+iIntros "_ WHITES".
+rred.
+
+iPoseProof ("Q" with "[ST_TGT]") as "Q".
+iFrame.
+iPoseProof ("Q" with "[i0PTR]") as "Q".
+iFrame.
+
+iPoseProof ("Q" with "[]") as "Q".
+
+{ iPureIntro. eauto. }
+
+iPoseProof ("Q" with "[i1MEM]") as "Q". iFrame.
+
+iFrame.
+Qed.
 
 
 Lemma wload_fun_spec'
